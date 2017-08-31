168f131b714e7d47f2a2ea951709e85fb81bf80e
Ghislain Rodrigues <git@ghislain-rodrigues.fr>
2017-08-31 12:53:37 +0100

    ball module added

--- js/pong2.js
+++ js/pong2.js
@@ -1,13 +1,12 @@
+var __DEBUG__ = false;
 loader.executeModule('main',
-'B', 'canvas', 'screenSize', 'settings',
-function (B, canvas, screenSize, settings) {
+'B', 'canvas', 'screenSize', 'Ball', 'settings',
+function (B, canvas, screenSize, Ball, settings) {
 	"use strict";
 
 	let context = canvas.getContext();
 	let timePreviousFrame;
 	let resources = {};
-
-	let __DEBUG__ = false;
 
 	let balls = [];
 	let targets = [];
@@ -58,17 +57,7 @@ function (B, canvas, screenSize, settings) {
 	}
 
 	function createBall() {
-		let ball = {
-			x: Math.random() * canvas.getWidth(),
-			y: Math.random() * canvas.getHeight(),
-			radius: 20,
-			speed: settings.BALL_CRUISE_SPEED,
-			direction: 0,
-			boost: false,
-			frozen: false,
-			frozenCountDown: 0
-		};
-		balls.push(ball);
+		balls.push(Ball.create());
 	}
 
 	function createTarget(ball) {
@@ -103,56 +92,14 @@ function (B, canvas, screenSize, settings) {
 	}
 
 	function update() {
-		updateBallFrozen();
-		updateBallDirection();
-		updateBallPosition();
+		balls[0].update(targets);
 		updateTarget();
 
-		testCollision();
-	}
-
-	function updateBallFrozen() {
-		if (balls[0].frozen) {
-			balls[0].frozenCountDown--;
-			if (balls[0].frozenCountDown <= 0) {
-				balls[0].frozenCountDown = 0;
-				balls[0].frozen = false;
-			}
+		let collidedTarget = balls[0].isColliding(targets);
+		if (collidedTarget) {
+			targets.splice(0, 1);
+			createTarget(balls[0]);
 		}
-	}
-
-	function updateBallDirection() {
-		// update ball direction
-		let directionToBall = (2 * Math.PI + Math.atan2(
-			targets[0].y - balls[0].y,
-			targets[0].x - balls[0].x
-		)) % (2 * Math.PI);
-		if (directionToBall != balls[0].direction) {
-			balls[0].direction += directionToBall > balls[0].direction ? settings.BALL_ANGULAR_SPEED : -settings.BALL_ANGULAR_SPEED;
-		}
-
-		if (balls[0].boost) {
-			balls[0].speed += 0.4;
-		}
-		if (balls[0].speed > settings.BALL_MAX_BOOST_SPEED) {
-			balls[0].boost = false;
-		}
-		if (!balls[0].boost && balls[0].speed > settings.BALL_CRUISE_SPEED) {
-			balls[0].speed -= 0.1;
-		}
-		else if (balls[0].speed < settings.BALL_CRUISE_SPEED) {
-			balls[0].speed = settings.BALL_CRUISE_SPEED;
-		}
-	}
-
-	function updateBallPosition() {
-		// update ball position
-		let speedVector = {
-			x: Math.cos(balls[0].direction) * balls[0].speed,
-			y: Math.sin(balls[0].direction) * balls[0].speed
-		};
-		balls[0].x = (canvas.getWidth() + balls[0].x + speedVector.x) % canvas.getWidth();
-		balls[0].y = (canvas.getHeight() + balls[0].y + speedVector.y) % canvas.getHeight();
 	}
 
 	function updateTarget() {
@@ -167,73 +114,17 @@ function (B, canvas, screenSize, settings) {
 		}
 	}
 
-	function testCollision() {
-		if (balls[0].frozen) {
-			return;
-		}
-
-		let distanceBallTarget = Math.sqrt(
-			Math.pow(targets[0].y - balls[0].y, 2) +
-			Math.pow(targets[0].x - balls[0].x, 2)
-		);
-
-		if (distanceBallTarget < targets[0].radius + balls[0].radius) {
-			if (targets[0].expands) {
-				balls[0].direction = (balls[0].direction + Math.PI) % (Math.PI * 2)
-				balls[0].frozen = true;
-				balls[0].frozenCountDown = 60;
-			}
-			else {
-				targets.splice(0, 1);
-				createTarget(balls[0]);
-			}
-		}
-	}
-
 	function draw() {
 		context.clearRect(0, 0, canvas.getWidth(), canvas.getHeight());
 
 		context.beginPath();
 		//draw ball
-		drawBall(balls[0], balls[0].x, balls[0].y);
-
-		if (balls[0].x < balls[0].radius) {
-			drawBall(balls[0], balls[0].x + canvas.getWidth(), balls[0].y);
-		}
-		if (balls[0].x + balls[0].radius > canvas.getWidth()) {
-			drawBall(balls[0], balls[0].x - canvas.getWidth(), balls[0].y);
-		}
-		if (balls[0].y < balls[0].radius) {
-			drawBall(balls[0], balls[0].x, balls[0].y + canvas.getHeight());
-		}
-		if (balls[0].y + balls[0].radius > canvas.getHeight()) {
-			drawBall(balls[0], balls[0].x, balls[0].y - canvas.getHeight());
-		}
+		balls[0].draw();
 
 		//draw target
 		context.moveTo(targets[0].x, targets[0].y);
 		context.arc(targets[0].x, targets[0].y, targets[0].radius, 0, 2 * Math.PI, false);
 		context.fill();
-
-		// draw speed vector
-		if (__DEBUG__) {
-			let speedVector = {
-				x: Math.cos(balls[0].direction) * balls[0].speed * 10,
-				y: Math.sin(balls[0].direction) * balls[0].speed * 10
-			};
-			context.beginPath();
-			context.moveTo(balls[0].x, balls[0].y);
-			context.strokeStyle = 'red';
-			context.lineTo(balls[0].x + speedVector.x, balls[0].y + speedVector.y);
-			context.stroke();
-		}
-	}
-
-	function drawBall(ball, x, y) {
-		//draw ball
-		context.fillStyle = 'black';
-		context.moveTo(x, y);
-		context.arc(x, y, ball.radius, 0, 2 * Math.PI, false);
 	}
 
 	function setEvents() {
